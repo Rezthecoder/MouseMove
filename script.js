8b5d91120d8e666038eb41109c56de1f7ff79f3f
Suresh <suresh@gmail.com>
2022-11-18 00:12:46 +0900

    range added

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let mouseXstart = 0;
+let mouseXstart = 300;
 let mouseXend = window.innerWidth;
 let mouseCurrent =0;
+let range = mouseXend - mouseXstart;
+
 
 
 
@@ -9,8 +11,8 @@ let mouseCurrent =0;
 const mousemove = (e)=> {
 //    console.log(`x is 
 //     +${e.clientX}`)
-    mouseCurrent = e.clientX;
-    fraction = mouseCurrent / mouseXend;
+    mouseCurrent = e.clientX -300;
+    fraction = mouseCurrent / range;
     console.log(fraction);
     console.log(mouseXend);
     console.log(mouseCurrent);
